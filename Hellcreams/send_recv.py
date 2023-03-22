03a6a40ad1f3f7c9023670b95a7218bb79b427ab
LuTheCat <cconny2002@gmail.com>
2023-03-22 17:30:59 +0900

    .

--- Hellcreams/send_recv.py
+++ Hellcreams/send_recv.py
@@ -4,3 +4,22 @@ class TcpConnect:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
+        self.model = None
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def client_connect(self):
+        if self.model is not None:
+            raise ConnectionError("This socket is already " + self.model + ".")
+
+        self.socket.connect((self.ip, self.port))
+
+    def server_open(self):
+        if self.model is not None:
+            raise ConnectionError("This socket is already " + self.model + ".")
+
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((self.ip, self.port))
+
+    async def server_listen(self):
+        self.socket.listen()
+        return self.socket.accept()
