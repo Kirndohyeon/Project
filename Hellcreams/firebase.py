a1b2b1b6cb93614d35043e26f098d66fbe81c130
Hellcreams <47096303+Hellcreams@users.noreply.github.com>
2023-04-06 18:01:55 +0900

    .

---
+++ Hellcreams/firebase.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred_object = credentials.Certificate('./capstonetest-d81da-firebase-adminsdk-64uiw-ba419847e3.json')
+default_app = firebase_admin.initialize_app(cred_object, {
+    'databaseURL': "https://capstonetest-d81da-default-rtdb.firebaseio.com"
+})
+ref = db.reference("users")
+ref.update({
+    "phew":
+        {"email": "qery@phew.com",
+         "id": "seki",
+         "password":"alali"}
+})
